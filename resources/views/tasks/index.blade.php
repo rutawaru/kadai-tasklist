206787d14e6ffdd1b27ab83df127227581a73388
wataru.higuchi <rutawaru@gmail.com>
2018-02-10 02:46:19 +0000

    add status collum and varidation

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -5,7 +5,7 @@
     <h1>タスク一覧</h1>
     
     @foreach ($tasks as $task)
-        <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}</li>
+        <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->status }} > {{ $task->content }}</li>
     @endforeach
     
     {!! link_to_route('tasks.create', '新規タスクの作成') !!}
