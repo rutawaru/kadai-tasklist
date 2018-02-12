c0b3386ed7921556c4106fb167b2aee0ffb022ea
wataru.higuchi <rutawaru@gmail.com>
2018-02-12 11:20:57 +0000

    tasklist bootstrap

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -4,10 +4,25 @@
 
     <h1>タスク一覧</h1>
     
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>ステータス</th>
+                <th>タスク</th>
+            </tr>
+        </thead>
+    
     @foreach ($tasks as $task)
-        <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->status }} > {{ $task->content }}</li>
+    <tr>
+        <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+        <td>{{ $task->status }} </td>
+        <td>{{ $task->content }}</td>
+    </tr>
     @endforeach
     
-    {!! link_to_route('tasks.create', '新規タスクの作成') !!}
+    </table>
+    
+    {!! link_to_route('tasks.create', '新規タスクの作成', null, ['class' => 'btn btn-primary'])!!}
 
 @endsection
